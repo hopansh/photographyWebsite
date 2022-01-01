1624ac74d44b4ccde9e172522e084d854278990f
hopansh <hopanshgahlot@gmail.com>
2022-01-01 23:13:53 +0530

    Added Instagram Feed

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,79 +1,84 @@
 import React from "react";
-import { ImageList, ImageListItem } from "@mui/material";
-import sample from "../../assets/images/sample.jpg";
+// import { ImageList, ImageListItem } from "@mui/material";
+// import sample from "../../assets/images/sample.jpg";
 
 function Home() {
   return (
-    <ImageList
-      // sx={{ width: 500, height: 450 }}
-      cols={Math.round(window.innerWidth / 400)}
-      rowHeight={200}
-      variant="quilted"
-      gap={20}
-    >
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <div
+      className="embedsocial-hashtag"
+      data-ref="b9329b1f70ed1dc152f65939304c1bd7f3ec5fa9"
+    ></div>
+
+    // <ImageList
+    //   // sx={{ width: 500, height: 450 }}
+    //   cols={Math.round(window.innerWidth / 400)}
+    //   rowHeight={200}
+    //   variant="quilted"
+    //   gap={20}
+    // >
+    //   {itemData.map((item) => (
+    //     <ImageListItem key={item.img}>
+    //       <img
+    //         src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+    //         srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+    //         alt={item.title}
+    //         loading="lazy"
+    //       />
+    //     </ImageListItem>
+    //   ))}
+    // </ImageList>
   );
 }
 
 export default Home;
 
-const itemData = [
-  {
-    img: sample,
-    title: "Breakfast",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-  },
-];
+// const itemData = [
+//   {
+//     img: sample,
+//     title: "Breakfast",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+//     title: "Burger",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+//     title: "Camera",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+//     title: "Coffee",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+//     title: "Hats",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+//     title: "Honey",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+//     title: "Basketball",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+//     title: "Fern",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+//     title: "Mushrooms",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+//     title: "Tomato basil",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+//     title: "Sea star",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+//     title: "Bike",
+//   },
+// ];
