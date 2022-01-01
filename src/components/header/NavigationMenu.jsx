898b2b3c40d39dc52caf28d06c6fff4e65f2b7e1
hopansh <hopanshgahlot@gmail.com>
2022-01-02 00:04:37 +0530

    Home Navigation Fix

--- src/components/header/NavigationMenu.jsx
+++ src/components/header/NavigationMenu.jsx
@@ -1,11 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 const NavigationMenu = ({ classes }) => {
   const menu = [
-    {
-      name: "Home",
-      path: "/",
-    },
     {
       name: "About",
       path: "/about",
@@ -15,14 +11,30 @@ const NavigationMenu = ({ classes }) => {
       path: "/contact",
     },
   ];
+  const [localIsActive, setlocalIsActive] = useState(true);
   return (
     <div className={classes.menu}>
+      <a
+        href="https://www.hopanshphotography.ga"
+        className={classes.item}
+        style={
+          localIsActive
+            ? {
+                paddingBottom: "0.4rem",
+                borderBottom: "0.1rem solid orange",
+              }
+            : {}
+        }
+      >
+        Home
+      </a>
       {menu.map((item, index) => (
         <NavLink
           to={item.path}
           className={classes.item}
           key={index}
           style={({ isActive }) => {
+            if (isActive) setlocalIsActive(false);
             return {
               paddingBottom: isActive ? "0.4rem" : "",
               borderBottom: isActive ? "0.1rem solid orange" : "",
