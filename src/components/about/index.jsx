1deb96408476822b294e20d4691c722656922da2
hopansh <hopanshgahlot@gmail.com>
2022-01-15 00:56:18 +0530

    "Updated Contact & about page"

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,29 +1,42 @@
-import { Grid } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-import React from "react";
+import { Grid } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import psnl_bg from '../../assets/images/psnl_bg.jpg';
+import React from 'react';
 const useStyle = makeStyles({
   root: {
-    height: "90vh",
+    height: '90vh',
   },
   aboutText: {
-    display: "flex",
-    alignItems: "center",
-    background: "rgba(0,0,0,0.5)",
-    color: "white",
-    padding: "3rem 2rem",
-    lineHeight: "2",
+    display: 'flex',
+    alignItems: 'center',
+    background: 'rgba(0,0,0,0.5)',
+    color: 'white',
+    padding: '3rem 2rem',
+    lineHeight: '2',
   },
   aboutImage: {
-    fontSize: "8rem",
-    fontWeight: "100",
-    textTransform: "uppercase",
+    fontSize: '8rem',
+    fontWeight: '100',
+    textTransform: 'uppercase',
   },
+  // bgImage: {
+  //   position: 'absolute',
+  //   width: '100%',
+  //   height: '100%',
+  //   top: 0,
+  //   left: 0,
+  //   opacity: '0.2',
+  //   zIndex: -1,
+  //   objectFit: 'cover',
+  // },
 });
 const About = () => {
   const classes = useStyle();
   return (
     <div>
-      <Grid container alignItems="center" className={classes.root}>
+      {/* <img className={classes.bgImage} src={psnl_bg} alt='bgImage' /> */}
+
+      <Grid container alignItems='center' className={classes.root}>
         <Grid item xs={12} lg={6}>
           <div className={classes.aboutImage}>
             <div>About</div>
@@ -31,17 +44,10 @@ const About = () => {
         </Grid>
         <Grid item xs={12} lg={6}>
           <div className={classes.aboutText}>
-            Pharetra magna ac placerat vestibulum lectus mauris ultrices. Ut
-            pharetra sit amet aliquam id diam maecenas ultricies mi.
-            Sollicitudin ac orci phasellus egestas tellus rutrum. Venenatis cras
-            sed felis eget. Amet risus nullam eget felis eget nunc. Lacus
-            vestibulum sed arcu non odio euismod. Consectetur adipiscing elit
-            duis tristique.Pharetra magna ac placerat vestibulum lectus mauris
-            ultrices. Ut pharetra sit amet aliquam id diam maecenas ultricies
-            mi. Sollicitudin ac orci phasellus egestas tellus rutrum. Venenatis
-            cras sed felis eget. Amet risus nullam eget felis eget nunc. Lacus
-            vestibulum sed arcu non odio euismod. Consectetur adipiscing elit
-            duis tristique.
+            k... so there's not much to tell about me, basically am a web
+            developer who do portrait photography as a hobby. Working as an
+            intern at a Fin-tech company and I am in my last semester of B.Tech
+            degree. I guess that's all about me.
           </div>
         </Grid>
       </Grid>
--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,7 +1,72 @@
-import React from "react";
-
+import { makeStyles } from '@mui/styles';
+import React from 'react';
+import {
+  Instagram,
+  Language,
+  LinkedIn,
+  Email,
+  GitHub,
+} from '@mui/icons-material';
+import { Grid, Typography } from '@mui/material';
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    justifyContent: 'center',
+  },
+  hrefItem: {
+    display: 'flex',
+    justifyContent: 'center',
+    color: 'black',
+    fontSize: '4rem',
+    textDecoration: 'none',
+    padding: '0.5rem 0.4rem',
+    margin: '1.5rem 38vw',
+    '&:hover': {
+      borderBottom: '0.1rem solid orange',
+      paddingBottom: '0.4rem',
+    },
+  },
+  icon: {
+    fontSize: '2.5rem !important',
+  },
+});
 function Contact() {
-  return <div>Contact Information will be updated Soon</div>;
+  const classes = useStyles();
+  const contact = [
+    {
+      icon: <Instagram className={classes.icon} />,
+      link: 'https://www.instagram.com/hopansh_photography/',
+    },
+    {
+      icon: <Email className={classes.icon} />,
+      link: 'mailto:hopansh.gahlot@gmail.com',
+    },
+    {
+      icon: <Language className={classes.icon} />,
+      link: 'www.hopanshphotography.ga',
+    },
+    {
+      icon: <LinkedIn className={classes.icon} />,
+      link: 'https://www.linkedin.com/in/hopansh-gahlot-596406183/',
+    },
+    {
+      icon: <GitHub className={classes.icon} />,
+      link: 'http://github.com/hopansh',
+    },
+  ];
+  return (
+    <Grid container justifyContent='center'>
+      {contact.map((el) => {
+        return (
+          <Grid item xs={12}>
+            <a href={el.link} className={classes.hrefItem}>
+              {el.icon}
+            </a>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 }
 
 export default Contact;
